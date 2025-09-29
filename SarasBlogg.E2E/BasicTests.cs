b3f155522625a92846b0018d38502c6db9c9fab3
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-09-29 13:32:55 +0200

    test(e2e): initial Playwright setup with smoke test

---
+++ SarasBlogg.E2E/BasicTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace SarasBlogg.E2E;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Self)]
+public class BasicTests : PageTest
+{
+    // Tillåt dev-cert lokalt
+    public override BrowserNewContextOptions ContextOptions()
+        => new() { IgnoreHTTPSErrors = true };
+
+    // Avkommentera för att se browsern när du felsöker:
+    // public override BrowserTypeLaunchOptions LaunchOptions() => new() { Headless = false };
+
+    [Test]
+    public async Task HomePage_ShouldHave_Title()
+    {
+        var baseUrl = TestContext.Parameters.Get("BaseUrl", "https://localhost:7130");
+        await Page.GotoAsync(baseUrl);
+        var title = await Page.TitleAsync();
+        TestContext.WriteLine($"Title: {title}");
+        Assert.That(title, Does.Contain("med Hjärtat som Kompass").IgnoreCase);
+
+    }
+}
